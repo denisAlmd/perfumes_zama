6f525b8069fdcb239e96b643180695eabe043a64
Denis Almeida <dnis.almeida@gmail.com>
2026-02-28 21:18:58 -0300

    Alterações 28 fevereiro

--- src/app/models/perfume.model.ts
+++ src/app/models/perfume.model.ts
@@ -2,12 +2,16 @@ export interface Perfume {
   id: number;
   nome: string;
   marca: string;
-  categoria: 'masculino' | 'feminino' | 'unissex';
+  categoria: string;
   volume: string;
-  preco: number;
-  descricao: string;
+  descricao?: string;
   imagens: string[];
   nota: number;
+  notas_saida?: string;
+  notas_coracao?: string;
+  notas_fundo?: string;
   destaque?: boolean;
   lancamento?: boolean;
+  fixacao?: string;
+  projecao?: string;
 }
